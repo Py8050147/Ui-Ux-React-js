aa55665369efbf7a444035bdda26d0ce57d59efb
Pankaj Kumar <94382374+Py8050147@users.noreply.github.com>
2024-09-07 01:49:31 +0530

    "Updated Dashboard component with minor layout and styling changes"

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ function Dashboard() {
   return (
     <div className="container ring-1 ring-gray-800  max-w-4xl md:max-w-full   px-20 ">
           <div className="ring-1 ring-gray-800 w-full  min-h-screen px-16  py-20 flex align-middle justify-between space-x-10 relative overflow-hidden">
-              <div className="text-white w-2/3 ml-14 py-24">
+              <div className="text-white w-2/3 ml-10 py-24">
                   <div className='mb-12'>
                       <p className='text-sm font-extralight tracking-widest text-gray-300 dark:text-gray-400 mb-10'>Level up with</p>
                       <h2 className=' text-6xl'>
@@ -19,8 +19,8 @@ function Dashboard() {
                             </svg>
                       </div>
                       <div className='mb-10'> 
-                          <h3 className='text-xl font-normal mb-5'>1200+ Premium Apps</h3>
-                          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                          <h3 className='text-xl font-light mb-3'>1200+ Premium Apps</h3>
+                          <p className="text-sm font-medium leading-relaxed max-w-lg text-gray-400 dark:text-gray-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
                       </div>
                   </div>
                   <div className="flex align-middle justify-center gap-5">
@@ -30,14 +30,34 @@ function Dashboard() {
                             </svg>
                       </div>
                       <div>
-                      <h3 className='text-xl font-normal mb-5'>Control Directly from Dashboard</h3>
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                      <h3 className='text-xl font-light mb-3'>Control Directly from Dashboard</h3>
+                      <p className="text-sm font-medium leading-relaxed max-w-lg text-gray-400 dark:text-gray-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
                       </div>
                   </div>
+                  <br className='ring-1 ring-gray-600 w-full h-2 border' />
+                  {/* <div className="inline-flex items-center pt-6 mt-8 border-t border-gray-800 sm:pt-10 sm:mt-14">
+          <span className="ml-2 text-base font-normal text-white"> 42 new design inspiration was added last week </span> */}
+                  <div className=' w-full inline-flex items-center pt-6 border-t border-gray-800 sm:pt-10 sm:mt-8'>
+                      {/* <div> */}
+                <div className=" relative inline-flex items-center justify-center mt-8 sm:mt-0 group">
+            <div className=" absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
+            <a
+              href="#"
+              title=""
+              className="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full"
+              role="button"
+            >
+              {" "}
+               Explore more{" "}
+            </a>
+          </div>
+                      </div>
+                      {/* </div> */}
               </div>
               <div className="text-white w-full h-[830px]  relative bg-gradient-to-r from-cyan-500 to-purple-500 rounded-lg">
-                  <img src={mokup} alt="mokup.png" className='max-w-6xl h-[750px] absolute top-10 left-16'/>
+                  <img src={mokup} alt="mokup.png" className='max-w-6xl min-h-lvh absolute top-10 left-14'/>
               </div>
+
       </div>
     </div>
   )
